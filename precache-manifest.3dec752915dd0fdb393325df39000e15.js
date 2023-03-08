a3f89ab2e65d13b9251ead5fb83394a2fbd139e7
Spence Riggs <SpeRig99@sccpss.com>
2023-03-08 06:47:11 -0500

    Updates

---
+++ precache-manifest.3dec752915dd0fdb393325df39000e15.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06a735ab7ab8187f89b975cfaaaf8ec",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "711dcf252070941b259e",
+    "url": "/emojipedia/static/js/2.0fcace88.chunk.js"
+  },
+  {
+    "revision": "f6174dce9d123d805ec5",
+    "url": "/emojipedia/static/js/main.39715fca.chunk.js"
+  },
+  {
+    "revision": "97d53470ee8f011916e5",
+    "url": "/emojipedia/static/js/runtime-main.8aea4f8f.js"
+  }
+]);
